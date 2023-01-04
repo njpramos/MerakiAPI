e7a0f3b04ea4d1043d4bc0427e4a0ac941fbb3f5
Nomer Ramos <nomer.ramos@acrom.ro>
2023-01-04 10:29:11 +0800

    updated

--- switch_backup.py
+++ switch_backup.py
@@ -71,44 +71,51 @@ def get_device_switchport(serial):
 
     return switchport_list
 
+# Main function
+
+def main():
+    try:
+        for network_id in network_id_list:
+            devices = get_network_devices(network_id)
+            #print("\n ================ " + get_network_name(devices[0]['networkId']) + " ================ \n")
+            if len(devices) != 0:
+                network_name = get_network_name(devices[0]['networkId'])
+                new_path = path + r'\\' + network_name
+                if network_name.find("LAB") == -1:
+                    if not os.path.exists(new_path): # Create directory for CMN sites
+                       os.makedirs(new_path)
+                    for device in devices:
+                        if device['model'].find('MS') != -1: # Extract only CMN MS switch devices
+                        #print(type(device))
+                            if "name" in device: # Execute if the switch has a label/hostname
+                                #print("Switch Name: " + device['name'] + " \n " + "Serial No.: " + device['serial'] + " \n", get_device_switchport(device['serial']))
+                                file_path = os.path.join(new_path + '\\', device['name'] + ".txt") 
+                                with open(file_path, "w") as text_file:
+                                    device_list = get_device_switchport(device['serial'])
+                                    text_file.write(device['serial'] + "\n\n" + "".join(str(f) for f in device_list))
+                                   
+                            else:
+                                #print("Switch Name: " + device['mac'] + " \n " + "Serial No.: " + device['serial'] + " \n ", get_device_switchport(device['serial']))
+                                file_path = os.path.join(new_path + '\\', device['mac'] + ".txt")
+                                with open(file_path, "w") as text_file:
+                                    device_list = get_device_switchport(device['serial'])
+                                    text_file.write(device['mac'] + "\n\n" + "".join(str(f) for f in device_list))
+    except KeyboardInterrupt:
+        print("Program Interrupted")
+    except:
+        print("There was an error during the program execution")
+ 
+ 
 # Initializes the program and parsing the organization ID from the CLI
 
-parser = argparse.ArgumentParser(prog = '[py | python3] switch_backup.py', description = 'Extracts swichport configurations for all the networks in an organization and save it to a text file', epilog= 'Help')
+parser = argparse.ArgumentParser(prog = '[py | python3] switch_backup.py', description = 'Extracts swichport configurations for all the networks in an organization and save it to a text file', epilog = 'i.e. py switch_backup.py -o 123456')
 
-parser.add_argument('-o', '--organization', default='464068', help = 'A valid Meraki organization ID')
+parser.add_argument('-o', '--organization', help = 'A valid Meraki organization ID')
 
 args = parser.parse_args()
 
-network_id_list = get_network_id(args.organization) # Call the function to extract all the Network IDs and store in a list
-
-try:
-    for network_id in network_id_list:
-        devices = get_network_devices(network_id)
-        #print("\n ================ " + get_network_name(devices[0]['networkId']) + " ================ \n")
-        if len(devices) != 0:
-            network_name = get_network_name(devices[0]['networkId'])
-            new_path = path + r'\\' + network_name
-            if network_name.find("LAB") == -1:
-                if not os.path.exists(new_path): # Create directory for CMN sites
-                   os.makedirs(new_path)
-                for device in devices:
-                    if device['model'].find('MS') != -1: # Extract only CMN MS switch devices
-                    #print(type(device))
-                        if "name" in device: # Execute if the switch has a label/hostname
-                            #print("Switch Name: " + device['name'] + " \n " + "Serial No.: " + device['serial'] + " \n", get_device_switchport(device['serial']))
-                            file_path = os.path.join(new_path + '\\', device['name'] + ".txt") 
-                            with open(file_path, "w") as text_file:
-                                device_list = get_device_switchport(device['serial'])
-                                text_file.write(device['serial'] + "\n\n" + "".join(str(f) for f in device_list))
-                               
-                        else:
-                            #print("Switch Name: " + device['mac'] + " \n " + "Serial No.: " + device['serial'] + " \n ", get_device_switchport(device['serial']))
-                            file_path = os.path.join(new_path + '\\', device['mac'] + ".txt")
-                            with open(file_path, "w") as text_file:
-                                device_list = get_device_switchport(device['serial'])
-                                text_file.write(device['mac'] + "\n\n" + "".join(str(f) for f in device_list))
-except KeyboardInterrupt:
-    print("Program Interrupted")
-except:
-    print("There was an error during the program execution")
-    
+if args.organization != None:
+    network_id_list = get_network_id(args.organization) # Call the function to extract all the Network IDs and store in a list
+    main() # Invoke the main function
+else:
+    parser.print_help()
